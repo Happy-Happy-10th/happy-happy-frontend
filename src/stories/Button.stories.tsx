6de00384fc1e5231b89c489a7b3c02a9389346bf
jaehyungz <blue259@hanmail.net>
2025-07-09 14:51:21 +0900

    feat: 스토리북 초기 설정

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button';
+import type { Meta, StoryObj } from '@storybook/nextjs';
+
+import { fn } from 'storybook/test';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: <>버튼</>,
+  },
+};
