bfe1177070689c620637eb8b801c56cedfa29489
kimtaehun-dev <changetodev@gmail.com>
2025-07-20 23:31:02 +0900

    fix :  스토리북 public 경로 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,23 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  addons: [],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "staticDirs": [
-    {
-      from: '../public',
-      to: '/happy-happy-frontend'
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    // 배포 환경(GitHub Actions)에서만 publicPath 수정
+    if (process.env.NODE_ENV === 'production' && config.output) {
+      config.output.publicPath = '/happy-happy-frontend/';
     }
-  ],
+    return config;
+  },
 };
+
 export default config;
