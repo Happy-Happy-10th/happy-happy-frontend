0d20a8095166831c0fb916e5e05fcd496f2daef8
kimtaehun-dev <changetodev@gmail.com>
2025-08-01 18:39:40 +0900

    feat : 스토리북 뷰포트 추가

--- src/stories/components/BottomTab.stories.tsx
+++ src/stories/components/BottomTab.stories.tsx
@@ -1,5 +1,7 @@
 import { BottomTab } from '@/components/features';
 import type { Meta, StoryObj } from '@storybook/nextjs';
+import { MINIMAL_VIEWPORTS } from 'storybook/viewport';
+import Layout from '../test/Layout';
 
 
 interface BottomTabStoryArgs {
@@ -10,39 +12,19 @@ interface BottomTabStoryArgs {
 const meta: Meta<typeof BottomTab> = {
   title: 'Components/BottomTab',
   component: BottomTab,
-  args: {
-    deviceWidthPx: 1200,
-    deviceHeightPx: 800,
-  },
-  argTypes: {
-    deviceWidthPx: {
-      control: { type: 'range', min: 383, max: 1280, step: 10 },
-    },
-    deviceHeightPx: {
-      control: { type: 'range', min: 800, max: 870, step: 10 },
-    },
-  },
 };
 
 export default meta;
-type Story = StoryObj<typeof meta & { args: BottomTabStoryArgs }>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
     deviceWidthPx: 1200
   },
 
-  render: (args) => (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div
-        style={{
-          width: `${args.deviceWidthPx}px`,
-          height: `${args.deviceHeightPx}px`,
-        }}
-        className="border border-solid flex flex-col justify-end"
-      >
-        <BottomTab />
-      </div>
+  render: () => (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <BottomTab />
     </div>
   )
 };
