6766b64ba906d7c37092cb5df113bf862a0b96ce
kimtaehun-dev <changetodev@gmail.com>
2025-08-06 02:12:42 +0900

    feat : feed page 기본 레이아웃 구성

---
+++ src/components/layouts/FeedPage.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/utils/tailwind-utils";
+import {Text} from "@/components/base"
+
+
+export default function FeedPage(){
+  return(
+    <div className={cn("h-full w-hull flex xl:flex-row flex-col gap-5 p-7.5 bg-yoteyo-gray-100")}>
+      {/* 오늘 일정 + 웰컴 문구 */}
+      <div className="flex flex-col gap-5">
+        <div className="xl:w-123 w-full xl:h-17 bg-yoteyo-main rounded-t-[8px] rounded-r-[8px] flex items-center justify-center">
+          <Text className="text-white" variant={"body1"}>요때요 맞춤 오늘의 특별한 일정을 지금 바로 확인해보세요!</Text>
+        </div>
+
+        <div className="xl:w-177 w-full p-6 min-h-100 bg-white rounded-[8px] flex-1 flex flex-col justify-between">
+          <div>
+            <Text variant={"title2"}>오늘 일정</Text>
+            <div>hi</div>
+            {/* 일정 배열 받으면 보여줄 컴포넌트 추가 */}
+          </div>
+          <div className="w-full h-12 flex justify-center items-center border-1 border-yoteyo-outline rounded-[8px]">
+            <Text variant={"body3"}>오늘 일정 더 보기</Text>
+          </div>
+        </div>
+      </div>
+      {/* 다가오는 일정 리스트 */}
+      <div className="xl:w-123 w-full flex-1 bg-white rounded-[8px] p-6">
+        <div className="flex flex-row justify-between items-center">
+          <Text variant={"title2"}>다가오는 일정</Text>
+          <div className="w-22 h-7 flex justify-center items-center rounded-[8px] border-1 border-yoteyo-outline">
+            <Text variant={"body3"}>전체보기</Text>
+          </div>
+        </div>
+        <div>hi</div>
+      </div>
+    </div>
+  )
+}
---
+++ src/stories/pages/Feed.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import TestCalendar from '../test/TestCalendar';
+import ServiceLayout from '../../components/layouts/ServiceLayout';
+import FeedPage from '@/components/layouts/FeedPage';
+
+const meta: Meta<typeof FeedPage> = {
+  title: 'pages/FeedPage',
+  component: FeedPage,
+};
+
+export default meta;
+type Story = StoryObj<typeof FeedPage>;
+
+export const feedPage: Story = {
+  render: (args) => (
+    <div className="w-screen h-screen">
+      <ServiceLayout>
+        <FeedPage/>
+      </ServiceLayout>
+    </div>
+  )
+};
