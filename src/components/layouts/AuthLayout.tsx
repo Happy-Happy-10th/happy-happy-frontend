5636047fb33c019c86478cd3d62dc3b7d4951948
jaehyungz <blue259@hanmail.net>
2025-08-05 18:18:38 +0900

    feat: AuthLayout height 수정

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const AuthLayout = ({ children }: Props) => {
   return (
-    <Box className="w-screen h-dvh bg-yoteyo-gray-100">
-      <Box className="w-full flex-col md:w-[600px] h-full bg-white mx-auto shadow-[0_0_12px_rgba(0,0,0,0.1)]">
+    <Box className="w-screen min-h-dvh bg-yoteyo-gray-100">
+      <Box className="relative flex-col w-full min-h-dvh md:w-[600px] bg-white mx-auto shadow-[0_0_12px_rgba(0,0,0,0.1)]">
         {children}
       </Box>
     </Box>
