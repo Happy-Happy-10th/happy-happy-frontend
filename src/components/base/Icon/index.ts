6566c8ec8f2b269ecd478c8ad1fd86aee6b990b5
jaehyungz <blue259@hanmail.net>
2025-08-05 18:19:27 +0900

    feat: Icon 컴포넌트 업데이트

--- src/components/base/Icon/index.ts
+++ src/components/base/Icon/index.ts
@@ -2,3 +2,5 @@ export * from './Icon';
 
 export { default as SplashIcon } from '../../../../public/icons/splash-icon.svg?react';
 export { default as SplashTextIcon } from '../../../../public/icons/splash-text-icon.svg?react';
+export { default as CheckIcon } from '../../../../public/icons/check.svg?react';
+export { default as CloseIcon } from '../../../../public/icons/close.svg?react';
