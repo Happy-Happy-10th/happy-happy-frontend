e8c94ad831d44f9af9c42cc8dbf0fd0cd1b68c38
jaehyungz <blue259@hanmail.net>
2025-08-06 10:23:00 +0900

    feat: 로그인 활성여부 store 구현

--- src/@types/auth.ts
+++ src/@types/auth.ts
@@ -3,18 +3,20 @@ type useSignInPayload = {
   password: string;
 };
 
+type MemberInfo = {
+  memberId: string;
+  username: string;
+  nickname: string;
+  imageUrl: string;
+};
+
 type useSignInResponse = {
   data: {
     accessToken: string;
     refreshToken: null | string;
-    memberInfo: {
-      memberId: string;
-      username: string;
-      nickname: string;
-      imageUrl: string;
-    };
+    memberInfo: MemberInfo;
   };
   status: number;
   message: string;
 };
-export type { useSignInPayload, useSignInResponse };
+export type { useSignInPayload, useSignInResponse, MemberInfo };
