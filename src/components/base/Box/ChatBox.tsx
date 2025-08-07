9fdaa54ac7eadf3de62f5e428136e30a2ee71998
jaehyungz <blue259@hanmail.net>
2025-08-07 23:41:22 +0900

    feat: chat ui 구현중

--- src/components/base/Box/ChatBox.tsx
+++ src/components/base/Box/ChatBox.tsx
@@ -11,15 +11,15 @@ interface Props {
 }
 
 function ChatBox({ message, from }: Props) {
-  const sentenceVariants = {
-    hidden: {},
-    visible: { opacity: 1, transition: { staggerChildren: 0.05 } },
-  };
+  // const sentenceVariants = {
+  //   hidden: {},
+  //   visible: { opacity: 1, transition: { staggerChildren: 0.05 } },
+  // };
 
-  const letterVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { opacity: { duration: 0 } } },
-  };
+  // const letterVariants = {
+  //   hidden: { opacity: 0 },
+  //   visible: { opacity: 1, transition: { opacity: { duration: 0 } } },
+  // };
 
   return (
     <>
@@ -32,15 +32,18 @@ function ChatBox({ message, from }: Props) {
           )}
         >
           {from === 'AI' ? (
-            <motion.p key={message} variants={sentenceVariants} initial="hidden" animate="visible">
-              {message.split('').map((char, i) => (
-                <motion.span key={`${char}-${i}`} className="whitespace-pre-wrap" variants={letterVariants}>
-                  <Text variant="body2" className="font-normal">
-                    {char}
-                  </Text>
-                </motion.span>
-              ))}
-            </motion.p>
+            // <motion.p key={message} variants={sentenceVariants} initial="hidden" animate="visible">
+            //   {message.split('').map((char, i) => (
+            //     <motion.span key={`${char}-${i}`} className="whitespace-pre-wrap" variants={letterVariants}>
+            //       <Text variant="body2" className="font-normal">
+            //         {char}
+            //       </Text>
+            //     </motion.span>
+            //   ))}
+            // </motion.p>
+            <Text variant="body2" className="font-normal whitespace-pre-wrap">
+              {message}
+            </Text>
           ) : (
             <Text variant="body2" className="font-normal whitespace-pre-wrap">
               {message}
