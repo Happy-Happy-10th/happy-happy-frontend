8f685e0dd35ba89df9eeff4cd8534dc15711687d
kimtaehun-dev <changetodev@gmail.com>
2025-08-07 21:17:54 +0900

    feat : GET, POST, PUT 호출 로직 개발

--- src/api/fragments/calendar/calendarFragments.ts
+++ src/api/fragments/calendar/calendarFragments.ts
@@ -1,12 +1,28 @@
 import { yoteyoAPI } from "@/api";
-import { GetEventsType } from "@/@types/calendar";
+import { CalendarEventType, GetEventsType } from "@/@types/calendar";
 
-export async function getUserEvents(){
-  return await yoteyoAPI<GetEventsType>('getCalendarEvents',{
+export async function getUserEvents(year:number){
+  return await yoteyoAPI<GetEventsType>('calendar/events',{
     method : 'get',
     headers:{
       'Content-Type': 'application/json',
     }
+  })
+}
+
+export async function postUserEvent(payload:CalendarEventType){
+  return await yoteyoAPI<GetEventsType>('calendar/events',{
+    method:'post',
+    headers:{'Content-Type': 'application/json'},
+    body : JSON.stringify({...payload})
+  })
+}
+
+export async function putUserEvent(payload:CalendarEventType){
+  return await yoteyoAPI<GetEventsType>(`calendar/events/${payload.id}`,{
+    method:'put',
+    headers:{'Content-Type': 'application/json'},
+    body : JSON.stringify({...payload})
   })
 }
 
