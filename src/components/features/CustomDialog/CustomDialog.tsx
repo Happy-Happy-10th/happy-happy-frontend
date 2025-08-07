c712546d7efa5c00dd4b79983e9ef9ae99762c07
kimtaehun-dev <changetodev@gmail.com>
2025-08-08 02:48:48 +0900

    feat : 다이얼로그 얼럿창 구현 및 샘플 작성

---
+++ src/components/features/CustomDialog/CustomDialog.tsx
@@ -0,0 +1,59 @@
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+  Box,
+  Button,
+  Text,
+  Icon,
+  AlertRedIcon,
+} from '@/components/base';
+import { X } from 'lucide-react';
+import { ReactNode } from 'react';
+
+type CustomDialogType = {
+  open : boolean
+  onClose:()=>void
+  icon : ReactNode
+  mainMsg ?: string;
+  subMsg ?: string;
+  btntext?:string;
+}
+export default function CustomDialog({
+  open, 
+  onClose,
+  icon=<AlertRedIcon/>, 
+  mainMsg, 
+  subMsg,
+  btntext="확인"
+}:CustomDialogType){
+  return(
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent showCloseButton={false} className='w-full'>
+        <DialogHeader>
+          <DialogClose asChild>
+            <Box className='w-full flex justify-end'>
+              <button type="button" onClick={onClose}>
+                <X size={24} />
+              </button>
+            </Box>
+          </DialogClose>
+        </DialogHeader>
+        <Box className='w-full flex flex-col justify-center items-center mb-13'>
+          <Icon className='w-24 h-24 mb-8'>
+            {icon}
+          </Icon>
+          <Text className='mb-5' variant={"title4"}>{mainMsg}</Text>
+          <Text variant={"body3"} className='text-yoteyo-gray-200'>{subMsg}</Text>
+        </Box>
+        <Button type='button' variant={'default'} onClick={onClose}>
+          <Text variant={"body3"}>{btntext}</Text>
+          </Button>
+      </DialogContent>
+    </Dialog>
+  )
+}
