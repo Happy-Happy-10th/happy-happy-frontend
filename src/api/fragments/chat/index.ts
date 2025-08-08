86a70fda9ab5d6d4cff553c3e3010126795df9fb
jaehyungz <blue259@hanmail.net>
2025-08-08 13:58:22 +0900

    feat: Chat API 연결중

--- src/api/fragments/chat/index.ts
+++ src/api/fragments/chat/index.ts
@@ -2,9 +2,17 @@ import { usePostAIMessagePayload } from '@/@types';
 import { yoteyoAPI } from '@/api/ky';
 
 export const PostAIMessage = async (payload: usePostAIMessagePayload) => {
+  const Requestbody = {
+    parameters: {
+      eventType: payload.parameters.eventType === 'online' ? '온라인' : '오프라인',
+      title: payload.parameters.title,
+      address: payload.parameters.address,
+    },
+  };
+
   return await yoteyoAPI<any>(`chat`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ...payload }),
+    body: JSON.stringify({ ...Requestbody }),
   });
 };
