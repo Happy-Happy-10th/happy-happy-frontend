92367fce0a7ef9d78fcf6b6d759f9f270bf3ad10
kimtaehun-dev <changetodev@gmail.com>
2025-08-17 23:41:15 +0900

    fix : 캘린더 이밴트 날짜 Date만 보이게 설정 및 Text 컴포넌트 적용

--- src/utils/calendar/dateConverter.ts
+++ src/utils/calendar/dateConverter.ts
@@ -60,10 +60,10 @@ function fixDate({ year, month, day, hour = 0, minute = 0 }: DateInput): Date {
 
 export function formatDateToString(date: Date): string {
   const year = date.getFullYear();
-  const month = date.getMonth()+1; // JS는 0부터 시작하므로 +1
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
 
   return `${year}.${month}.${day}T${hour}:${minute}`;
 }
