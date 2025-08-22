a43b0986ac4e6a4f84db4e24b3f260e3ad10bba3
jaehyungz <blue259@hanmail.net>
2025-08-22 13:36:36 +0900

    fix: 소셜로그인 이미 가입된 계정있을경우 예외처리

--- src/app/(pages)/oauth/callback/page.tsx
+++ src/app/(pages)/oauth/callback/page.tsx
@@ -1,12 +1,16 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQueryState, parseAsString, parseAsBoolean, useQueryStates } from 'nuqs';
 import { useRouter } from 'next/navigation';
+import { AlertRedIcon } from '@/components/base';
+import { CustomDialog } from '@/components/features';
 
 interface Props {}
 
 function Page() {
   const router = useRouter();
+
+  const [dialogState, setDialogState] = useState({ open: false, main: '', sub: '' });
 
   const [payload] = useQueryStates({
     success: parseAsBoolean.withDefault(false),
@@ -18,11 +22,29 @@ function Page() {
       router.push('/home');
     }
     if (payload.error === 'already_registered') {
-      router.push('/auth/login');
+      setDialogState({
+        open: true,
+        main: '이미 이메일로 가입된 계정이 있습니다.',
+        sub: '기존 계정으로 로그인해 주세요.',
+      });
     }
   }, [payload]);
 
-  return <></>;
+  return (
+    <>
+      <CustomDialog
+        open={dialogState.open}
+        onClose={() => {
+          setDialogState({ open: false, main: '', sub: '' });
+          router.push('/auth/login');
+        }}
+        icon={<AlertRedIcon />}
+        mainMsg={dialogState.main}
+        subMsg={dialogState.sub}
+        onSubmit={() => router.push('/auth/login')}
+      />
+    </>
+  );
 }
 
 export default Page;
