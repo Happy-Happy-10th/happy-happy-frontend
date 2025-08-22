3c7ce69876ca41c74789d78bc2231f21ecdfe488
kimtaehun-dev <changetodev@gmail.com>
2025-08-22 11:46:32 +0900

    refactor : react-big-calendar 컴포넌트 조합함수 base에 위치

--- src/components/features/CustomCalendar/CustomCalendar.tsx
+++ src/components/features/CustomCalendar/CustomCalendar.tsx
@@ -1,27 +1,20 @@
-'use client'
-import { useMemo } from "react";
-import { useMediaQuery } from "@/hooks";
-import { CalendarEventType, SetDateHandler } from "@/@types";
+'use client';
+import { useMemo } from 'react';
+import { useMediaQuery } from '@/hooks';
+import { CalendarEventType, SetDateHandler } from '@/@types';
 // react-big-calendar
-import { Components, SlotInfo, ToolbarProps } from "react-big-calendar";
+import { Components, SlotInfo, ToolbarProps } from 'react-big-calendar';
 // base Components
-import { 
-  CalendarGrid, 
-  CalendarToolbar, 
-  CalendarMonthHeader, 
-  CalendarDateHeader, 
-  CalendarEventDetail, 
-  CalendarEventMinimal 
-} from "@/components/base";
+import { CalendarGrid, makeCustomComponents } from '@/components/base';
 
 type CustomCalendarPropsType = {
-  className?:string;
-  viewDate ?: Date;
-  events ?: CalendarEventType[];
-  isMondayStart?:boolean;
-  onChangeViewDate ?: SetDateHandler;
-  onSlotSelected ?: (slot: SlotInfo) => void;
-}
+  className?: string;
+  viewDate?: Date;
+  events?: CalendarEventType[];
+  isMondayStart?: boolean;
+  onChangeViewDate?: SetDateHandler;
+  onSlotSelected?: (slot: SlotInfo) => void;
+};
 
 const NOOP_SET_DATE: SetDateHandler = () => {};
 
@@ -31,15 +24,16 @@ export function CustomCalendar({
   events = [],
   isMondayStart = true,
   onChangeViewDate = NOOP_SET_DATE,
-  onSlotSelected
-}:CustomCalendarPropsType){
+  onSlotSelected,
+}: CustomCalendarPropsType) {
   //뷰포트 감시
   const BREAKPOINT = '1000px' as const;
-  const isWide = useMediaQuery(`(min-width:${BREAKPOINT})`,true)
+  const isWide = useMediaQuery(`(min-width:${BREAKPOINT})`, true);
   // 전달할 커스텀 컴포넌트
   const components = useMemo<Components<CalendarEventType, object>>(
-    ()=> makeCustomComponents({isWide,onChangeViewDate}),
-    [isWide, onChangeViewDate]);
+    () => makeCustomComponents({ isWide, onChangeViewDate }),
+    [isWide, onChangeViewDate],
+  );
 
   return (
     <CalendarGrid
@@ -51,26 +45,5 @@ export function CustomCalendar({
       onSelectSlot={onSlotSelected}
       components={components}
     />
-  )
-}
-
-
-type OptionType = { isWide : boolean; onChangeViewDate :SetDateHandler}
-function makeCustomComponents({isWide,onChangeViewDate}:OptionType)
-:Components<CalendarEventType, object>{
-  return{
-    toolbar: (p:ToolbarProps<CalendarEventType>)=>(
-      <CalendarToolbar 
-        {...p}
-        onChangeViewDate={onChangeViewDate}
-        />
-    ),
-    month:{
-      dateHeader:CalendarDateHeader,
-      header:CalendarMonthHeader
-    },
-    event:(eventProps)=>isWide
-      ?<CalendarEventDetail {...eventProps.event} />
-      :<CalendarEventMinimal {...eventProps.event} />,
-  }
+  );
 }
