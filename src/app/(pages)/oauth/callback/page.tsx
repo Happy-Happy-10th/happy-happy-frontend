768c9c681e6665a44f7aaecddab71cbcd5bc17ac
jaehyungz <blue259@hanmail.net>
2025-08-22 13:11:25 +0900

    feat: 소셜로그인 연결

--- src/app/(pages)/oauth/callback/page.tsx
+++ src/app/(pages)/oauth/callback/page.tsx
@@ -1,11 +1,26 @@
 'use client';
-import React from 'react';
-import { useQueryState, parseAsString, parseAsBoolean } from 'nuqs';
+import React, { useEffect } from 'react';
+import { useQueryState, parseAsString, parseAsBoolean, useQueryStates } from 'nuqs';
+import { useRouter } from 'next/navigation';
 
 interface Props {}
 
 function Page() {
-  const [success] = useQueryState('success', parseAsBoolean.withDefault(false));
+  const router = useRouter();
+
+  const [payload] = useQueryStates({
+    success: parseAsBoolean.withDefault(false),
+    error: parseAsString.withDefault(''),
+  });
+
+  useEffect(() => {
+    if (payload.success) {
+      router.push('/home');
+    }
+    if (payload.error === 'already_registered') {
+      router.push('/auth/login');
+    }
+  }, [payload]);
 
   return <></>;
 }
