d15924a210667b84f146e2e999d4767582ad7c38
kimtaehun-dev <changetodev@gmail.com>
2025-08-27 22:28:22 +0900

    fix : SSR 컴포넌트에서 util쪽 함수 import시 발생하는 문제 제거

--- src/app/(pages)/shared/page.tsx
+++ src/app/(pages)/shared/page.tsx
@@ -1,35 +1,34 @@
-import { notFound } from "next/navigation";
-import { CalendarEventType } from "@/@types/calendar";
-import { 
-  Box,
-  Text
-} from "@/components/base";
-import { convertEventStringToDate, base64UrlDecodeToString } from "@/utils";
-import { UserEventForm } from "@/components/features";
+import { notFound } from 'next/navigation';
+import { CalendarEventType } from '@/@types/calendar';
+import { Box, Text } from '@/components/base';
 
-type SharedPageType = {searchParams: Promise<{ d?: string }>;}
-export default async function SharedPage({searchParams}:SharedPageType){
+import { UserEventForm } from '@/components/features';
+import { base64UrlDecodeToString } from '@/utils/calendar/URLBase64';
+import { convertEventStringToDate } from '@/utils/calendar/dateConverter';
+
+type SharedPageType = { searchParams: Promise<{ d?: string }> };
+export default async function SharedPage({ searchParams }: SharedPageType) {
   const { d } = await searchParams;
   if (!d) return notFound();
 
-  let event:CalendarEventType;
-  // try {
-  //   const json=base64UrlDecodeToString(d);
-  //   const raw = JSON.parse(json);
-  //   event = convertEventStringToDate(raw);
-  // } catch (e){
-  //   console.error("Invalid shared event", e);
-  //   return notFound();
-  // }
+  let event: CalendarEventType;
+  try {
+    const json = base64UrlDecodeToString(d);
+    const raw = JSON.parse(json);
+    event = convertEventStringToDate(raw);
+  } catch (e) {
+    console.error('Invalid shared event', e);
+    return notFound();
+  }
 
-  return(
+  return (
     <Box className="w-full h-full flex flex-col justify-center items-center bg-yoteyo-bg-default overflow-hidden">
       <Box className="bg-yoteyo-main xl:w-123 w-[80%] h-17 text-white flex justify-center items-center rounded-[8px]">
-        <Text variant={"title2"}>공유 받은 일정을 확인해보세요</Text>
+        <Text variant={'title2'}>공유 받은 일정을 확인해보세요</Text>
       </Box>
       <Box className="flex flex-col xl:w-123 w-[80%] flex-1 min-h-0 h-full overflow-auto">
-        <UserEventForm mode="shared"/>
+        <UserEventForm mode="shared" event={event} />
       </Box>
     </Box>
-  )
+  );
 }
