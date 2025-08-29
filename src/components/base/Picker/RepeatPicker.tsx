a62e5e5e62f36d4ae675559f4a4bfd75b88728da
kimtaehun-dev <changetodev@gmail.com>
2025-08-30 02:16:09 +0900

    fix : 디자인 누락된 부분 추가

--- src/components/base/Picker/RepeatPicker.tsx
+++ src/components/base/Picker/RepeatPicker.tsx
@@ -4,16 +4,13 @@ import clsx from 'clsx';
 import { cn } from '@/utils/tailwind-utils';
 import { RepeatType } from '@/@types';
 
-const repeatPickerFrame = clsx(
-  'w-full h-27 pt-5 pb-5 pl-4 pr-4',
-  'yoteyo-m-title-main-sm'
-);
+const repeatPickerFrame = clsx('w-full h-27 pt-5 pb-5 pl-4 pr-4', 'yoteyo-m-title-main-sm');
 
 const buttonStyle = clsx(
   'w-12 h-8 yoteyo-m-body-lg',
-  'border-1 border-soild border-yoteyo-gray-100 rounded-[8px]',
-  'flex justify-center items-center',
-  'hover:cursor-pointer'
+  'border-1 border-soild border-yoteyo-gray-400 rounded-[8px]',
+  'flex justify-center items-center text-yoteyo-gray-100',
+  'hover:cursor-pointer',
 );
 
 type PropsType = {
@@ -23,7 +20,7 @@ type PropsType = {
 
 export default function RepeatPicker({ value, onChange }: PropsType) {
   const handleSelected = (selected: RepeatType) => {
-    if (!onChange) return; 
+    if (!onChange) return;
     onChange(value === selected ? null : selected);
   };
 
@@ -33,22 +30,13 @@ export default function RepeatPicker({ value, onChange }: PropsType) {
     <div className={repeatPickerFrame}>
       <span>일정 반복</span>
       <div className="flex flex-row gap-4 mt-[18px]">
-        {options.map((option) => (
+        {options.map(option => (
           <div
             key={option}
-            className={cn(
-              buttonStyle,
-              value === option && 'bg-yoteyo-main text-white'
-            )}
+            className={cn(buttonStyle, value === option && 'bg-yoteyo-main text-white')}
             onClick={() => handleSelected(option)}
           >
-            {option === 'day'
-              ? '일'
-              : option === 'week'
-              ? '주'
-              : option === 'month'
-              ? '월'
-              : '년'}
+            {option === 'day' ? '일' : option === 'week' ? '주' : option === 'month' ? '월' : '년'}
           </div>
         ))}
       </div>
