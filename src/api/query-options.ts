9145ebd27d70a250f74379dab90a444081aded82
jaehyungz <blue259@hanmail.net>
2025-09-03 21:15:16 +0900

    fix: gctime 0 setting

--- src/api/query-options.ts
+++ src/api/query-options.ts
@@ -3,7 +3,11 @@ import { QueryClient } from '@tanstack/react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      gcTime: 0,
       retry: 0,
+    },
+    mutations: {
+      gcTime: 0,
     },
   },
 });
