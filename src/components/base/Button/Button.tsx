87b3043efe5ecf8754f548c6cca53dc32009dd6c
jaehyungz <blue259@hanmail.net>
2025-09-03 11:03:30 +0900

    feat: update button disabled icon variant

--- src/components/base/Button/Button.tsx
+++ src/components/base/Button/Button.tsx
@@ -4,13 +4,13 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/tailwind-utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl cursor-pointer text-sm  transition-all disabled:pointer-events-none disabled:bg-yoteyo-disabled disabled:text-yoteyo-gray-300  [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl cursor-pointer text-sm  transition-all disabled:pointer-events-none disabled:bg-yoteyo-disabled disabled:text-yoteyo-gray-300  [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none ",
   {
     variants: {
       variant: {
         default: 'text-lg/[1.5] font-bold bg-yoteyo-main text-white',
         outline: 'text-lg/[1.5] font-bold bg-white text-yoteyo-main border-2 border-yoteyo-main ',
-        icon: '',
+        icon: 'disabled:bg-transparent ',
       },
       size: {
         default: 'w-full h-14',
