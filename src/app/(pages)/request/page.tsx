5b8677d62406b3fed2fb4f8676d17d88a0376434
jaehyungz <blue259@hanmail.net>
2025-09-04 10:40:50 +0900

    feat: update request page [test]

--- src/app/(pages)/request/page.tsx
+++ src/app/(pages)/request/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSignIn } from '@/api';
 import { Box, Button, Text } from '@/components/base';
 import AuthLayout from '@/components/layouts/AuthLayout';
 import { useMutation } from '@tanstack/react-query';
@@ -11,24 +12,36 @@ function Page(props: Props) {
   const {} = props;
 
   const { mutate } = useMutation({
-    mutationKey: [''],
+    mutationKey: ['jsonplaceholder'],
     mutationFn: async () => {
       await fetch('https://jsonplaceholder.typicode.com/todos/1');
     },
   });
 
   const { mutate: apiRouteMutate } = useMutation({
-    mutationKey: [''],
+    mutationKey: ['NEXT-BFF'],
     mutationFn: async () => {
       await fetch('/api/health');
     },
   });
+
+  const { mutate: loginMutate } = useSignIn({});
 
   return (
     <AuthLayout>
       <Box className="flex flex-col gap-y-4 h-full">
         <Button onClick={() => mutate()}>jsonplaceholder 테스트</Button>
         <Button onClick={() => apiRouteMutate()}>API ROUTE 테스트</Button>
+        <Button
+          onClick={() =>
+            loginMutate({
+              userid: 'kidkid',
+              password: 'Qweasd123#',
+            })
+          }
+        >
+          yottaeyo 테스트
+        </Button>
       </Box>
     </AuthLayout>
   );
