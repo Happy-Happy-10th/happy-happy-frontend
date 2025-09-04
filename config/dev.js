a618a7cb0b6167e5816cc44197915d367958bf51
jaehyungz <blue259@hanmail.net>
2025-09-04 20:37:41 +0900

    feat: updated backend end-point

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   NEXT_PUBLIC_APP_ENV: 'DEV',
-  YOTEYO_API_URL: 'https://yottaeyo.site/api',
+  YOTEYO_API_URL: 'https://api.yottaeyo.site/api',
   NEXT_PUBLIC_KAKAO_JS_KEY: 'b61dc56053863ae2871eab1f4aa23bd9',
 };
